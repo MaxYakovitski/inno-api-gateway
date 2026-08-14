60c3093618804d05c72824d7ec7b1a3adf4e87a5
max-yakovitski <m.yakovitski@gmail.com>
2026-08-14 12:51:51 +0300

    Initial commit

---
+++ src/main/java/com/innowise/apigateway/InnoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoApiGatewayApplication.class, args);
+	}
+
+}
