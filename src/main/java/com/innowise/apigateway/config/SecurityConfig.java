d7206cb6d38d827185819e04a11bed5ad42849ea
max-yakovitski <m.yakovitski@gmail.com>
2026-08-14 17:08:17 +0300

    endpoints protection enabled

--- src/main/java/com/innowise/apigateway/config/SecurityConfig.java
+++ src/main/java/com/innowise/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.innowise.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -10,10 +11,23 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+  private static final String[] PUBLIC_ENDPOINTS = {
+          "/api/v1/auth/login",
+          "/api/v1/auth/register",
+          "/api/v1/auth/refresh",
+          "/actuator/health",
+          "/actuator/health/**"
+  };
+
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
     return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-        .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
+            .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+        .authorizeExchange(exchanges -> exchanges.pathMatchers(PUBLIC_ENDPOINTS).permitAll()
+                .anyExchange()
+                .authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
         .build();
   }
 }
